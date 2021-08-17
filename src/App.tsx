90c88677017dce7e8260ad50252fb565b9cdf01e
huanhulan <huanhulan83@gmail.com>
2021-08-17 21:10:09 +0800

    docs: add tips

--- src/App.tsx
+++ src/App.tsx
@@ -9,122 +9,126 @@ import { MyBuggyRadio } from "./MyBuggyRadio";
 
 function App() {
   return (
-    <div className="App">
-      <hr />
-      normal code below
-      <hr />
-      <div>
-        <MyForm name="test">
+    <>
+      <h1>Open React devtool to see re-renders</h1>
+      <div className="App">
+        <hr />
+        normal code below
+        <hr />
+        <div>
+          <MyForm name="test">
+            {new Array(10)
+              .fill(1)
+              .map((_, idx) => idx)
+              .map((v) => (
+                <MyRadio value={`${v}`} name="test" id={`test${v}`} key={v}>
+                  {v}
+                </MyRadio>
+              ))}
+          </MyForm>
+        </div>
+        <hr />
+        <div>
+          <MyForm name="test1">
+            {new Array(10)
+              .fill(1)
+              .map((_, idx) => idx)
+              .map((v) => (
+                <MyAnotherRadio
+                  value={`${v}`}
+                  name="test"
+                  id={`test1${v}`}
+                  key={v}
+                >
+                  {v}
+                </MyAnotherRadio>
+              ))}
+          </MyForm>
+        </div>
+        <hr />
+        <MyAnotherForm name="test3" />
+        <hr />
+        <strong>Bug reproduce</strong>: using div as form without name and radio
+        without onchange and defaultChecked
+        <hr />
+        <MyBuggyForm>
           {new Array(10)
             .fill(1)
             .map((_, idx) => idx)
             .map((v) => (
-              <MyRadio value={`${v}`} name="test" id={`test${v}`} key={v}>
+              // @ts-ignore
+              <MyBuggyRadio value={`${v}`} name="test4" key={v}>
+                {v}
+              </MyBuggyRadio>
+            ))}
+        </MyBuggyForm>
+        <hr />
+        <MyBuggyForm>
+          {new Array(10)
+            .fill(1)
+            .map((_, idx) => idx)
+            .map((v) => (
+              // @ts-ignore
+              <MyBuggyRadio value={`${v}`} name="test4" key={v}>
+                {v}
+              </MyBuggyRadio>
+            ))}
+        </MyBuggyForm>
+        <hr />
+        buggy code below: using div as form with a same name
+        <hr />
+        <MyBuggyForm name="test5">
+          {new Array(10)
+            .fill(1)
+            .map((_, idx) => idx)
+            .map((v) => (
+              // @ts-ignore
+              <MyRadio value={`${v}`} name="test5" key={v}>
                 {v}
               </MyRadio>
             ))}
-        </MyForm>
-      </div>
-      <hr />
-      <div>
-        <MyForm name="test1">
+        </MyBuggyForm>
+        <hr />
+        <MyBuggyForm name="test5">
           {new Array(10)
             .fill(1)
             .map((_, idx) => idx)
             .map((v) => (
-              <MyAnotherRadio
-                value={`${v}`}
-                name="test"
-                id={`test1${v}`}
-                key={v}
-              >
-                {v}
-              </MyAnotherRadio>
-            ))}
-        </MyForm>
-      </div>
-      <hr />
-      <MyAnotherForm name="test3" />
-      <hr />
-      <strong>Bug reproduce</strong>: using div as form without name and radio without onchange and defaultChecked
-      <hr />
-      <MyBuggyForm>
-        {new Array(10)
-          .fill(1)
-          .map((_, idx) => idx)
-          .map((v) => (
-            // @ts-ignore
-            <MyBuggyRadio value={`${v}`} name="test4" key={v}>
-              {v}
-            </MyBuggyRadio>
-          ))}
-      </MyBuggyForm>
-      <hr />
-      <MyBuggyForm>
-        {new Array(10)
-          .fill(1)
-          .map((_, idx) => idx)
-          .map((v) => (
-            // @ts-ignore
-            <MyBuggyRadio value={`${v}`} name="test4" key={v}>
-              {v}
-            </MyBuggyRadio>
-          ))}
-      </MyBuggyForm>
-      <hr />
-      buggy code below: using div as form with name
-      <hr />
-      <MyBuggyForm name="test5">
-        {new Array(10)
-          .fill(1)
-          .map((_, idx) => idx)
-          .map((v) => (
-            // @ts-ignore
-            <MyRadio value={`${v}`} name="test5" key={v}>
-              {v}
-            </MyRadio>
-          ))}
-      </MyBuggyForm>
-      <hr />
-      <MyBuggyForm name="test5">
-        {new Array(10)
-          .fill(1)
-          .map((_, idx) => idx)
-          .map((v) => (
-            // @ts-ignore
-            <MyRadio value={`${v}`} name="test5" key={v}>
-              {v}
-            </MyRadio>
-          ))}
-      </MyBuggyForm>
-      <hr />
-      buggy code below: duplicate form
-      <hr />
-      <div>
-        <MyForm name="test6">
-          {new Array(10)
-            .fill(1)
-            .map((_, idx) => idx)
-            .map((v) => (
-              <MyRadio value={`${v}`} name="test6" id={`test6${v}`} key={v}>
+              // @ts-ignore
+              <MyRadio value={`${v}`} name="test5" key={v}>
                 {v}
               </MyRadio>
             ))}
-        </MyForm>
+        </MyBuggyForm>
+        <hr />
+        buggy code below: duplicate form
+        <hr />
+        <div>
+          <MyForm name="test6">
+            {new Array(10)
+              .fill(1)
+              .map((_, idx) => idx)
+              .map((v) => (
+                <MyRadio value={`${v}`} name="test6" id={`test6${v}`} key={v}>
+                  {v}
+                </MyRadio>
+              ))}
+          </MyForm>
+        </div>
+        <div>
+          <MyForm name="test6">
+            {new Array(10)
+              .fill(1)
+              .map((_, idx) => idx)
+              .map((v) => (
+                <MyRadio value={`${v}`} name="test6" id={`test6${v}`} key={v}>
+                  {v}
+                </MyRadio>
+              ))}
+          </MyForm>
+        </div>
       </div>
-      <div>
-        <MyForm name="test6">
-          {new Array(10)
-            .fill(1)
-            .map((_, idx) => idx)
-            .map((v) => (
-              <MyRadio value={`${v}`} name="test6" id={`test6${v}`} key={v}>
-                {v}
-              </MyRadio>
-            ))}
-        </MyForm>
-      </div>
-    </div>
+    </>
   );
 }
 
